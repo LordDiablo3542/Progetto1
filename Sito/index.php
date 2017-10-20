0dade93652de009293d697ada1dcb8e828f652b1
LordDiablo3542 <31542527+LordDiablo3542@users.noreply.github.com>
2017-10-20 23:20:24 +0200

    a

--- Sito/index.php
+++ Sito/index.php
@@ -23,41 +23,43 @@
 		td,th{
 			width: 25%;
 		}
-		div.title{
-			background-color: DodgerBlue;
-			height: 100px;
+		.title{
+			background-color: #00cc99;
 			margin: 0px;
+			text-align: center;
+			color: white;
+			padding:15px 0px ;
+
 		}
 		th,td {
 			padding-top: 20px;
 		}
 		#bottom {
-			background-color: #000000;
+			background-color: #333333;
 			color: white;
 			text-align: center;
 			height: 100px;
 			margin: 0px;
 		}
-		#minorenne {
-			display: none;
-		}
-		#minorenne , x:-moz-any-link{
-			display: block;
-		}
 		.subtitle {
 			text-align: left;
-			background-color: lightblue;
+			color: white;
+			background-color: #00e6ac;
 			padding-left: 15px;
 			border-radius: 10px;
 		}
 	</style>
 </head>
-<body>
+<body onload="ifMinorenne(), situazioneAttuale(), aprendistato()">
 
 	<?php
-		$oservazioni = $nome = $cognome = $sesso = $dnascita = $email = $via = $citta = $cap =$nazione = $nCellulare = $nTelefono = $cantone = $sedeUS = $annoUS = $denominazione = $tipo ="";
+		$osservazioni = $nome = $cognome = $sesso = $dnascita = $email = $via = $citta = $cap =$nazione = $nCellulare = $nTelefono = $cantone ="";
 
-		$nomeP = $cognomeP = $ruolo = $emailP = $viaP = $cittaP = $capP = $cantoneP = $nazioneP = $nCellulareP = $nTelefonoP =  "-";
+		$nomeP = $cognomeP = $ruolo = $emailP = $viaP = $cittaP = $capP = $cantoneP = $nazioneP = $nCellulareP = $nTelefonoP =  "";
+
+		$tipo = $denominazione = $sedeUS = $annoUS = "";
+
+		$prof = $scuola = $sede = $annoS = $liv = $annoA = $professione = "";
 
 		$nazione = $nazioneP = $nazionalita = "Svizzera";
 		$cantone = "Ticino";
@@ -76,7 +78,7 @@
 			$nazione = test_input($_POST["nazione"]);
 			$nCellulare = test_input($_POST["nCellulare"]);
 			$nTelefono = test_input($_POST["nTelefono"]);
-			$oservazioni = test_input($_POST["oservazioni"]);
+			$osservazioni = test_input($_POST["osservazioni"]);
 
 			$nomeP = test_input($_POST["nomeP"]);
 			$cognomeP = test_input($_POST["cognomeP"]);
@@ -95,12 +97,31 @@
 			$sedeUS = test_input($_POST["sedeUS"]);
 			$annoUS = test_input($_POST["annoUS"]);
 
-			$text = "$nome;$cognome;$dnascita;$sesso;$nazionalita;$via;$cap;$citta;$cantone;$nazione;$nCellulare;$nTelefono;$email;$nomeP;$cognomeP;$ruolo;$viaP;$cittaP;$capP;$cantoneP;$nazioneP;$nCellulareP;$nTelefonoP;$emailP;$oservazioni";
-			$myFile= file_put_contents("dati.csv", $text . PHP_EOL, FILE_APPEND);
+			$prof = test_input($_POST["prof"]);
+			$scuola = test_input($_POST["scuola"]);
+			$sede = test_input($_POST["sede"]);
+			$annoS = test_input($_POST["annoS"]);
+			$liv = test_input($_POST["liv"]);
+			$annoA = test_input($_POST["annoA"]);
+			$professione = test_input($_POST["professione"]);
 
-			$oservazioni = $nome = $cognome = $sesso = $dnascita = $email = $via = $citta = $cap = $nazionalita = $cantone = $nazione = $nCellulare = $nTelefono = "";
+			$text = "\n$nome;$cognome;$dnascita;$sesso;$nazionalita;$via;$cap;$citta;$cantone;$nazione;$nCellulare;$nTelefono;$email;$nomeP;$cognomeP;$ruolo;$viaP;$capP;$cittaP;$cantoneP;$nazioneP;$nCellulareP;$nTelefonoP;$emailP;$tipo;$denominazione;$sedeUS;$annoUS;$prof;$scuola;$sede;$annoS;$professione;$liv;$annoA;$osservazioni";
 
-			$nomeP = $cognomeP = $ruolo = $emailP = $viaP = $cittaP = $capP = $cantoneP = $nazioneP = $nCellulareP = $nTelefonoP = "-";
+			$myfile = fopen("dati.csv", "a+") or die("Unable to open file!");
+			if(filesize("dati.csv") == 0){
+				$first = "Nome;Cognome;Data di nascita;Genere;Nazionalità;Via;CAP;Citta;Cantone;Nazione;Num. cellulare;Num. telefono;Email;Nome autorità parentale;Cognome autorità parentale;Ruolo;Via autorità parentale;CAP autorità parentale;Citta autorità parentale;Cantone autorità parentale;Nazione autorità parentale;Num. cellulare autorità parentale;Num. telefono autorità parentale;Email autorità parentale;Tipo ultima scuola;Denominazione ultima scuola;Sede ultima scuola;Anno ultima scuola;Professione;Scuola;Sede;Anno scolastico;Nome professione;Livello;Anno aprendistato;Osservazioni";
+				fwrite($myfile, $first);
+			}
+			fwrite($myfile, $text);
+			fclose($myfile);
+			
+			$osservazioni = $nome = $cognome = $sesso = $dnascita = $email = $via = $citta = $cap =$nazione = $nCellulare = $nTelefono = $cantone ="";
+
+			$nomeP = $cognomeP = $ruolo = $emailP = $viaP = $cittaP = $capP = $cantoneP = $nazioneP = $nCellulareP = $nTelefonoP =  "";
+
+			$tipo = $denominazione = $sedeUS = $annoUS = "";
+
+			$prof = $scuola = $sede = $annoS = $liv = $annoA = $professione = "";
 
 			$nazione = $nazioneP = $nazionalita = "Svizzera";
 			$cantone = "Ticino";
@@ -115,6 +136,7 @@
 	?>
 
 	<div class="title">
+		<h1>Dati dei visitatori di espoprofessione</h1>
 	</div>
 	<div class="panel">	
 		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">	
@@ -230,112 +252,108 @@
 						<input type="text" name="email" value="<?php echo $email?>"  required pattern="^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$">
 					</td>
 				</tr>
-				</table>
+			</table>
 
 
-				<div id="minorenne">
-				<table>
+			<table id="minorenne">
+				<tr>
+					<th class="subtitle" colspan="2">
+						<h3>Dati anagrafici dell'autorità parentale</h3>
+					</th>
+				</tr>
+				<tr>
+					<th>
+						 Nome :
+					</th>
+					<td>
+						<input type="text" name="nomeP" value="<?php echo $nomeP?>"  required pattern="^(?=\s*\S).*$" class="min">
+					</td>
+				</tr>	
+				<tr>
+					<th>
+						 Cognome :
+					</th>
+					<td>
+						<input type="text" name="cognomeP" value="<?php echo $cognomeP?>"  required pattern="^(?=\s*\S).*$" class="min">
+					</td>
+				</tr>	
+				<tr>
+					<th>
+						 Ruolo :
+					</th>
+					<td>
+						<select name="ruolo" class="min">
+						  	<option value="Padre" selected>Padre</option>
+						  	<option value="Madre">Madre</option>
+						  	<option value="Tutore">Tutore</option>
+						</select>
+					</td>
+				</tr>
 					<tr>
-						<th class="subtitle" colspan="2">
-							<h3>Dati anagrafici dell'autorità parentale</h3>
-						</th>
-					</tr>
-					<tr>
-						<th>
-							 Nome :
-						</th>
-						<td>
-							<input type="text" name="nomeP" value="<?php echo $nomeP?>"  required pattern="^(?=\s*\S).*$">
-						</td>
-					</tr>	
-					<tr>
-						<th>
-							 Cognome :
-						</th>
-						<td>
-							<input type="text" name="cognomeP" value="<?php echo $cognomeP?>"  required pattern="^(?=\s*\S).*$">
-						</td>
-					</tr>	
-					<tr>
-						<th>
-							 Ruolo :
-						</th>
-						<td>
-							<select name="ruolo">
-							  	<option value="Padre" selected>Padre</option>
-							  	<option value="Madre">Madre</option>
-							  	<option value="Tutore">Tutore</option>
-							</select>
-						</td>
-					</tr>
-
-					<tr>
-						<th>
-							Indirizzo :
-						</th>
-						<td>
-							<input type="text" name="viaP" value="<?php echo $viaP?>"  required pattern="^(?=\s*\S).*$">
-						</td>
-					</tr>
-					<tr>
-						<th>
-							CAP :
-						</th>
-						<td>
-							<input type="text" name="capP" value="<?php echo $capP?>" required pattern="^\d+$">
-						</td>
-					</tr>
-					<tr>
-						<th>
-							Città :
-						</th>
-						<td>
-							<input type="text" name="cittaP" value="<?php echo $cittaP?>" required pattern="^(?=\s*\S).*$">
-						</td>
-					</tr>
-					<tr>
-						<th>
-							Cantone/ provincia :
-						</th>
-						<td>
-							<input type="text" name="cantoneP" value="<?php echo $cantoneP?>"  required pattern="^(?=\s*\S).*$">
-						</td>
-					</tr>
-					<tr>
-						<th>
-							Nazione :
-						</th>
-						<td>
-							<input type="text" name="nazioneP" value="<?php echo $nazioneP?>"  required pattern="^(?=\s*\S).*$">
-						</td>
-					</tr>
-					<tr>
-						<th>
-							Numero di cellulare :
-						</th>
-						<td>
-							<input type="text" name="nCellulareP" value="<?php echo $nCellulareP?>" required pattern="^\d+$">
-						</td>
-					</tr>
-					<tr>
-						<th>
-							Numero di telefono di casa :
-						</th>
-						<td>
-							<input type="text" name="nTelefonoP" value="<?php echo $nTelefonoP?>" required pattern="^\d+$">
-						</td>
-					</tr>
-
-					<tr>
-						<th>
-							 E-mail :
-						</th>
-						<td>
-							<input type="text" name="emailP" value="<?php echo $emailP?>"  required pattern="^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$">
-						</td>
-					</tr>
-					</table>			
-				</div>
+					<th>
+						Indirizzo :
+					</th>
+					<td>
+						<input type="text" name="viaP" value="<?php echo $viaP?>"  required pattern="^(?=\s*\S).*$" class="min">
+					</td>
+				</tr>
+				<tr>
+					<th>
+						CAP :
+					</th>
+					<td>
+						<input type="text" name="capP" value="<?php echo $capP?>" required pattern="^\d+$" class="min">
+					</td>
+				</tr>
+				<tr>
+					<th>
+						Città :
+					</th>
+					<td>
+						<input type="text" name="cittaP" value="<?php echo $cittaP?>" required pattern="^(?=\s*\S).*$" class="min">
+					</td>
+				</tr>
+				<tr>
+					<th>
+						Cantone/ provincia :
+					</th>
+					<td>
+						<input type="text" name="cantoneP" value="<?php echo $cantoneP?>"  required pattern="^(?=\s*\S).*$" class="min">
+					</td>
+				</tr>
+				<tr>
+					<th>
+						Nazione :
+					</th>
+					<td>
+						<input type="text" name="nazioneP" value="<?php echo $nazioneP?>"  required pattern="^(?=\s*\S).*$" class="min">
+					</td>
+				</tr>
+				<tr>
+					<th>
+						Numero di cellulare :
+					</th>
+					<td>
+						<input type="text" name="nCellulareP" value="<?php echo $nCellulareP?>" required pattern="^\d+$" class="min">
+					</td>
+				</tr>
+				<tr>
+					<th>
+						Numero di telefono di casa :
+					</th>
+					<td>
+						<input type="text" name="nTelefonoP" value="<?php echo $nTelefonoP?>" required pattern="^\d+$" class="min">
+					</td>
+				</tr>
+				<tr>
+					<th>
+						 E-mail :
+					</th>
+					<td>
+						<input type="text" name="emailP" value="<?php echo $emailP?>"  required pattern="^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$" class="min">
+					</td>
+				</tr>
+			</table>
 
 			<table>
 				<tr>
@@ -394,9 +412,69 @@
 						Situazione attuale :
 					</th>
 					<td>
-						<input type="radio" name="prof" value="Studente" checked> Studente
+						<input type="radio" name="prof" value="Studente" onchange="situazioneAttuale()" class="situazione" checked> Studente
 						<br>
-						<input type="radio" name="prof" value="Lavoro"> Lavoro
+						<input type="radio" name="prof" value="Lavoro" onchange="situazioneAttuale()" class="situazione"> Lavoro
+					</td>
+				</tr>
+			</table>
+
+			<table id="lavoro">
+				<tr>
+					<th  style="width: 30%">
+						Professione :
+					</th>
+					<td>
+						<input type="text" name="professione" value="<?php echo $professione?>" required pattern="^(?=\s*\S).*$" class="lav">
+					</td>
+				</tr>
+
+				<tr>
+					<th valign="top">
+						Livello :
+					</th>
+					<td>
+						<input type="radio" name="liv" value="Apprendista"  class="lav" checked onchange="aprendistato()"> Apprendista
+						<br>
+						<input type="radio" name="liv" value="Prossionista"  class="lav" onchange="aprendistato()"> Professionista
+						<br>
+						<input type="radio" name="liv" value="Indipendente" class="lav" onchange="aprendistato()"> Indipendente
+					</td>
+				</tr>
+
+				<tr>
+					<th id="lav">
+						Anno aprendistato :
+					</th>
+					<td>
+						<input type="text" name="annoA" value="<?php echo $annoA?>" required pattern="[0-9]" class="lav">
+					</td>
+				</tr>
+			</table>
+
+			<table id="studente">
+				<tr>
+					<th style="width: 30%">
+						Scuola :
+					</th>
+					<td>
+						<input type="text" name="scuola" value="<?php echo $scuola?>" required pattern="^(?=\s*\S).*$" class="stu">
+					</td>
+				</tr>
+				<tr>
+					<th>
+						Sede :
+					</th>
+					<td>
+						<input type="text" name="sede" value="<?php echo $sede?>" required pattern="^(?=\s*\S).*$" class="stu">
+					</td>
+				</tr>
+				<tr>
+					<th>
+						Anno scolastico :
+					</th>
+					<td>
+						<input type="text" name="annoS" value="<?php echo $annoS?>" required pattern="[0-9]" class="stu">
 					</td>
 				</tr>
 			</table>
@@ -409,14 +487,14 @@
 					</th>
 				</tr>
 				<tr>
-					<td colspan="2">
-						<textarea name="osservazioni" maxlength="239" rows="5" cols="40"></textarea>
+					<td colspan="2" style="padding-right: 30px;">
+						<textarea name="osservazioni" maxlength="239" rows="5" style="width: 100%;" value="<?php echo $osservazioni?>"></textarea>
 					</td>
 				</tr>
 				
 				<tr>
 					<td></td>
-					<td colspan="2">
+					<td>
 						<input type="submit" name="submit" value="Invia">  
 					</td>
 				</tr>
@@ -432,12 +510,60 @@
 		function ifMinorenne(){
 			var oggi = new Date(Date.now()).getFullYear();
 			var d = document.getElementById("data").value.slice(6, 10);
-			alert(d);
+			var m = document.getElementsByClassName("min");
 			if(oggi-d < 18){
+				for (var i = m.length - 1; i >= 0; i--) {
+					m[i].disabled = false;
+				}
 				document.getElementById("minorenne").style.display = "block";
 			}
 			else{
+				for (var i = m.length - 1; i >= 0; i--) {
+					m[i].disabled = true;
+				}
 				document.getElementById("minorenne").style.display = "none";
+			}
+		}
+
+		function situazioneAttuale(){
+			var l = document.getElementById("lavoro");
+			var cl = document.getElementsByClassName("lav");
+			var s = document.getElementById("studente");
+			var cs = document.getElementsByClassName("stu");
+			var d = document.getElementsByName("prof");
+			if(d[0].checked){
+				for (var i = cl.length - 1; i >= 0; i--) {
+					cl[i].disabled = true;
+				}
+				for (var i = cs.length - 1; i >= 0; i--) {
+					cs[i].disabled = false;
+				}
+				l.style.display = "none";
+				s.style.display = "block";
+			}
+			else if(d[1].checked){
+				for (var i = cl.length - 1; i >= 0; i--) {
+					cl[i].disabled = false;
+				}
+				for (var i = cs.length - 1; i >= 0; i--) {
+					cs[i].disabled = true;
+				}
+				l.style.display = "block";
+				s.style.display = "none";
+			}
+		}
+
+		function aprendistato(){
+			var d = document.getElementsByName("liv");
+			var cl = document.getElementsByClassName("lav")[4];
+			var dl = document.getElementById("lav");
+			if(d[0].checked){
+				cl.disabled = false;
+				dl.style.color = "black";
+			}
+			else{
+				cl.disabled = true;
+				dl.style.color = "#CCCCCC";
 			}
 		}
 	</script>
